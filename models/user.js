1829f7e87c277234e92be33d0dab4b528d3fbd67
novalpradanash <novalpradana.sadianhuri@gmail.com>
2020-07-09 18:23:40 +0700

    MVC and sequelize skel

---
+++ models/user.js
@@ -0,0 +1,56 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models.UserRecipe)
+    }
+  };
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: "Please insert your Username!"
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: "Please insert your email!"
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: "Please insert your password!"
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+
+  User.beforeCreate((instance, options) => {
+    instance.password = hashPassword(instance.password)
+  })
+
+  
+  return User;
+};
---
+++ models/userrecipe.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class UserRecipe extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      UserRecipe.belongsTo(models.User)
+    }
+  };
+  UserRecipe.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: "Please insert the Name! Value cant be empty!"
+        }
+      }
+    },
+    ingredient: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: "Please insert the Ingredients! Value cant be empty!"
+        }
+      }
+    },
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'UserRecipe',
+  });
+  return UserRecipe;
+};
